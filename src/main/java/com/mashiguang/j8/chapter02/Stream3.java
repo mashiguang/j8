681071d0fde53108b79b044303050ac580b1b1f5
mashiguang <mashiguang0833@sina.com>
2017-11-20 09:18:15 +0800

    chapter2

--- src/main/java/com/mashiguang/j8/chapter02/Stream3.java
+++ src/main/java/com/mashiguang/j8/chapter02/Stream3.java
@@ -3,11 +3,10 @@ package com.mashiguang.j8.chapter02;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class Stream3 {
 
@@ -15,7 +14,7 @@ public class Stream3 {
     public void test() throws IOException {
 
         Stream<Locale> locals = Stream.of(Locale.getAvailableLocales());
-        Map<String, String> languageNames = locals.collect(Collectors.toMap(l -> l.getDisplayLanguage(),
+        Map<String, String> languageNames = locals.collect(toMap(l -> l.getDisplayLanguage(),
                 l -> l.getDisplayLanguage(l),
                 (existingValue, newValue) -> existingValue));
 
@@ -27,7 +26,7 @@ public class Stream3 {
 
         Stream<Locale> locals = Stream.of(Locale.getAvailableLocales());
         Map<String, Set<String>> languageNames = locals.collect(
-                Collectors.toMap(
+                toMap(
                         l -> l.getDisplayCountry(),
                         l -> Collections.singleton(l.getDisplayLanguage()),
                         (a, b) -> {
@@ -56,11 +55,11 @@ public class Stream3 {
         Locale[] locales = Locale.getAvailableLocales();
 
         //to List
-        Map<String, List<Locale>> country2List = Stream.of(locales).collect(Collectors.groupingBy(Locale::getDisplayCountry));
+        Map<String, List<Locale>> country2List = Stream.of(locales).collect(groupingBy(Locale::getDisplayCountry));
         country2List.forEach((key, list) -> System.out.println(key));
 
         //to Set
-        Map<String, Set<Locale>> country2set = Stream.of(locales).collect(Collectors.groupingBy(Locale::getDisplayCountry, Collectors.toSet()));
+        Map<String, Set<Locale>> country2set = Stream.of(locales).collect(groupingBy(Locale::getDisplayCountry, toSet()));
         country2set.forEach((key, list) -> System.out.println(key));
 
     }
@@ -70,7 +69,7 @@ public class Stream3 {
 
         Stream<Locale> locals = Stream.of(Locale.getAvailableLocales());
 
-        Map<Boolean, List<Locale>> enAndOthers = locals.collect(Collectors.partitioningBy(l -> "en".equals(l.getLanguage())));
+        Map<Boolean, List<Locale>> enAndOthers = locals.collect(partitioningBy(l -> "en".equals(l.getLanguage())));
 
         //所有说英语的国家
         enAndOthers.get(true).forEach(l -> System.out.println(l.getDisplayCountry()));
@@ -78,5 +77,20 @@ public class Stream3 {
         System.out.println("no english:");
         //不说英语的国家
         enAndOthers.get(false).forEach(l -> System.out.println(l.getDisplayCountry()));
+    }
+
+    @Test
+    public void test6() throws IOException {
+
+        Locale[] locales = Locale.getAvailableLocales();
+
+        //to Set
+        Map<String, Set<String>> country2set = Stream.of(locales).collect(groupingBy(l -> l.getDisplayCountry(),
+                mapping(l -> l.getDisplayLanguage(), toSet())));
+        country2set.forEach((key, list) -> {
+            System.out.print(key);
+            System.out.println(list);
+        });
+
     }
 }
