94f3ff5829ba20fc9c66a503c19999374ce93d3d
mashiguang <mashiguang0833@sina.com>
2018-12-14 10:25:48 +0800

    Optional

--- src/main/java/com/mashiguang/j8/chapter02/Optional01.java
+++ src/main/java/com/mashiguang/j8/chapter02/Optional01.java
@@ -1,6 +1,19 @@
 package com.mashiguang.j8.chapter02;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+
 public class Optional01 {
 
+    @Test
+    public void test() {
+        Optional<Integer> op = Optional.of(1);
+        Assert.assertTrue(op.isPresent());
+
+        Optional<Integer> op2 = Optional.ofNullable(null);
+        Assert.assertFalse(op2.isPresent());
+    }
 
 }
